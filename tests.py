b882f07b898c03bee427ac9443b5b3a9a32bd417
Yevgeny Arman <yevgeny.arman@forescout.com>
2020-03-04 10:49:16 +0200

    fisrt commit comment

--- tests.py
+++ tests.py
@@ -1,5 +1,13 @@
 import urllib.request
 import re
+import logging
+
+logging.basicConfig(filename="logger.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+logger=logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 
 url = 'https://levproperties.co.uk/'
 
@@ -7,31 +15,35 @@ req = urllib.request.Request(url)
 resp = urllib.request.urlopen(req)
 respData = resp.read()
 
+attributes = ["src", "title"]
 
+# first commit
 
 def value_from_splitted_string(splitted_string, expression_I_am_looking_for):
     here_your_string = ""
     what_I_am_looking_for = []
     list_of_what_I_am_looking_for = [j for j in splitted_string if expression_I_am_looking_for in j]
     # print("this is what I am looking for: ")
-    print(list_of_what_I_am_looking_for)
+    logger.debug(f'List of values that I search: {list_of_what_I_am_looking_for}')
     if len(list_of_what_I_am_looking_for) != 0:
-        what_I_am_looking_for = re.findall(r''+expression_I_am_looking_for+'"(.*)"',list_of_what_I_am_looking_for[0])
+        what_I_am_looking_for = re.findall(r'^'+expression_I_am_looking_for+'="(.*)"',list_of_what_I_am_looking_for[0])
     if len(what_I_am_looking_for) != 0:
-        here_your_string=what_I_am_looking_for[0]
+        here_your_string = what_I_am_looking_for[0]
+        logger.debug(f'Here is your String: {here_your_string}')
     return here_your_string
 
 
-def dict_format(url,title):
+def dict_format(attributes,attribute_value_list):
     dict_format={}
-    if url == "" and title!="":
-        dict_format = {}
-    elif title == "" and url!="":
-        dict_format = {}
-    elif title == "" and url == "":
-        dict_format={}
-    else:
-        dict_format= {"url":url,"title":title}
+    for i in attribute_value_list:
+        if i== "":
+            dict_format = {}
+            logger.warning("this is empty dictionary")
+            break
+        else:
+            dict_format=dict(zip(attributes, attribute_value_list))
+            logger.debug("This is dict format: ")
+            logger.debug(dict_format)
     return dict_format
 
 def get_value_from_list(list):
@@ -43,13 +55,16 @@ def get_value_from_list(list):
 
 
 test = [
-    {"input":"<img    title=\"ttl\" src=\"url\" >",
+    {"input":"<img    title=\"ttl\" src=\"url\"  >",
      "output":[
          {
-             "url": "url",
-             "title": "ttl"
+             "src": "url",
+             "title": "ttl",
          },
      ]},
+    {"input":"<img    title=\"ttl\" asrc=\"url\" >",
+            "output":[
+    ]},
     {"input":"<img   xxx=\"src=\" >",
      "output":[
      ]},
@@ -63,11 +78,11 @@ test = [
     {"input": "<img src=\"url\"  dfdfd dfdf title=\"title\"> <img src=\"xxx\" title=\"yyy\">",
      "output": [
          {
-             "url": "url",
+             "src": "url",
              "title": "title"
          },
          {
-             "url": "xxx",
+             "src": "xxx",
              "title": "yyy"
          }
      ]
@@ -75,11 +90,11 @@ test = [
     {"input": "<img src=\"url\" title=\"title\"> <img src=\"xxx\" title=\"yyy\">",
      "output": [
          {
-             "url": "url",
+             "src": "url",
              "title": "title"
          },
          {
-             "url": "xxx",
+             "src": "xxx",
              "title": "yyy"
          }
      ]
@@ -90,70 +105,43 @@ def link_dictionary(lev_web_page):
     img_links_and_alt = re.findall(r'<img\s+.*?>',str(lev_web_page))
     link_dics_input = []
     for i in img_links_and_alt:
+        attribute_value_list=[]
         split_array = i.split()
-        url1 = value_from_splitted_string(split_array,"src=")
-        print(url1)
-        title1 = value_from_splitted_string(split_array,"title=")
-        print(title1)
-        # dict_format = {"url":url[0],"title":title[0]}
-        dictionary = dict_format(url1,title1)
+        for j in attributes:
+            attribute_value = value_from_splitted_string(split_array,j)
+            logger.debug(f'[link dictionary] This is attribute value: {attribute_value} ')
+            attribute_value_list.append(attribute_value)
+        dictionary = dict_format(attributes,attribute_value_list)
         if dictionary:
             link_dics_input.append(dictionary)
-        print("this is link_dics: ")
-        print(link_dics_input)
+        logger.debug("this is link_dics: ")
+        logger.debug(link_dics_input)
     return link_dics_input
 
 
-# print(link_dictionary("<img    title=\"ttl\" src=\"url\" >"))
 
-# link_dictionary("<img    title=\"ttl\" src=\"url\" >")
-
-# example_split_string = "<img    title=\"ttl\" src=\"url\" >"
-# example_split_array = example_split_string.split()
-# print(example_split_array)
-# src_list = [i for i in example_split_array if "src=" in i]
-# title_list = [i for i in example_split_array if "title=" in i]
-#
-#
-# print(src_list)
-# for i in src_list:
-#     url = re.findall(r'src=(.*)',i)
-#
-# print(url)
-#
-# for i in title_list:
-#     title = re.findall(r'title=(.*)',i)
-#
-# print(title)
-
-
-
-#link_dictionary(respData)
-#for i in link_dictionary(respData):
-#   print(i["url"])
-#  print(i["title"])
 
 
 for i in test:
     input = link_dictionary(i["input"])
-    print("This is input:")
-    print(input)
-    print("This is output:")
-    print(i["output"])
+    logger.debug("This is input:")
+    logger.debug(input)
+    logger.debug("This is output:")
+    logger.debug(i["output"])
     index=0
     if len(input)==len(i["output"]) == 0:
-        print("The input and the output are empty")
+        logger.error("The input and the output are empty")
         continue
     if len(input)==len(i["output"]):
-        print(len(input),len(i["output"]))
+        logger.debug((len(input) , len(i["output"])))
         for j in input:
-            if ("url"or"title") in j and ("url" or "title") in i["output"][index] and j["url"] == i["output"][index]["url"] and j["title"]==i["output"][index]["title"]:
-                print("success")
+            if ("src"or"title") in j and ("src" or "title") in i["output"][index] and j["src"] == i["output"][index]["src"] and j["title"]==i["output"][index]["title"]:
+                logger.debug(" Great success")
                 index =index + 1
             else:
-                print("values dont match")
+                logger.error("values dont match")
     else:
-        print("the lenth of the lists doesn't match")
+        logger.error("the lenth of the lists doesn't match")
 
 
 
